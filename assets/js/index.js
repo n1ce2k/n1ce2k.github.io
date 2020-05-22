38bfd0dfd52fbd7458b44204737dd98ae3b12015
n1ce2k <andre_n1ce2k@users.noreply.github.com>
2020-05-22 12:26:28 +0300

    asd

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,18 +8,15 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
 function topFunction() {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
 }
-
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
-
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
